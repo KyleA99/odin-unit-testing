4c8abed201373bda95082b32573eaca4e537acb0
Kyle Angelich <kyleangelich1@gmail.com>
2024-01-03 19:24:54 -0800

    Add capitalize-string tests

---
+++ capitalize-string.test.js
@@ -0,0 +1,25 @@
+const capitalize = require("./capitalize-string");
+
+test("capitalize the first character of a string", () => {
+  const inputString = "hello world";
+  const capitalizedString = capitalize(inputString);
+  expect(capitalizedString).toBe("Hello world");
+});
+
+test("capitalize empty string returns empty string", () => {
+  const inputString = "";
+  const capitalizedString = capitalize(inputString);
+  expect(capitalizedString).toBe("");
+});
+
+test("capitalize single character string", () => {
+  const inputString = "a";
+  const capitalizedString = capitalize(inputString);
+  expect(capitalizedString).toBe("A");
+});
+
+test("capitalize string with all uppercase characters", () => {
+  const inputString = "HELLO";
+  const capitalizedString = capitalize(inputString);
+  expect(capitalizedString).toBe("HELLO");
+});
